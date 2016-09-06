2192060e7a54efde11f5171a176a48f9927ebf06
Adam Dempsey <adamdemps@gmail.com>
2016-09-06 17:16:34 -0500

    All mg routines added.

--- src/multigrid.c
+++ src/multigrid.c
@@ -5,6 +5,7 @@ extern void restrict2D(double *,  double *,  int );
 extern void solve(double *, double *, int);
 extern void gauss_seidel(double *, double *, int, int);
 extern void prolongate2D(double *, double *, int );
+extern void residual(double *, double *, double *, int);
 
 void multigrid(double *u, double *f, const int lmin, const int lmax, const int numcycles, const int numpre, const int numpost) {
 /* Multigrid V-cycle for the Poisson equation 
@@ -55,7 +56,7 @@ void multigrid(double *u, double *f, const int lmin, const int lmax, const int n
     for(l=lmin;l<lmax;l++) {
 
         /* Move solution up a level */
-        prolongate2D(sol[l+1], sol[l], l);
+        prolongate2D(sol[l+1-lmin], sol[l-lmin], l);
     
         /* Vcycles */
         for(i=0; i < numcycles; i++) {
@@ -66,18 +67,21 @@ void multigrid(double *u, double *f, const int lmin, const int lmax, const int n
                  */
                 gauss_seidel(sol[j-lmin],rhs[j-lmin], j,numpre);
 
-                resid(res[j-lmin], sol[j-lmin], rhs[j-lmin], j);
+                residual(res[j-lmin], sol[j-lmin], rhs[j-lmin], j);
 
                 restrict2D(rhs[j-lmin-1],res[j-lmin],j-1);
 
-                matfill(sol[j-lmin-1], NULL, j-1);
+                matfill(sol[j-lmin-1], NULL, 1 + (2 << (j-1)));
             }
             /* Solve at lowest level */
             solve(sol[0],rhs[0], 0);
 
             /* Going up */
             for(j=1; j<l; j++) {
-                addint(sol[j-lmin], sol[j-1-lmin], res[j-lmin], j);
+                prolongate2D(sol[j-lmin],sol[j-1-lmin],j);
+                for(k=0;k<1 + (2 << j);k++) {
+                    sol[j-lmin][k] += res[j-lmin][k];
+                }
                 gauss_seidel(sol[j-lmin],rhs[j-lmin], j,numpost);
             }
         }
@@ -85,7 +89,6 @@ void multigrid(double *u, double *f, const int lmin, const int lmax, const int n
 
 
             
-    }
 
     
 
